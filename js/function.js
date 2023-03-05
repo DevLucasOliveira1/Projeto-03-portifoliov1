3c8d5d9f553da80ed9fe5cdc906c5ec573ef2580
Lks.Programação <124909098+Lks-programacao@users.noreply.github.com>
2023-03-04 22:36:11 -0300

    ATT 2.0
    Adicionado:
    >> Página de projetos - imagens; css ; javacript.
    Melhorias:
    >> Melhorias no responsivo em geral.
    >> Foi adicionado links nos blocos de rede social.
    >> Foi feita uma limpeza no código css, pra ficar mais clean.

--- js/function.js
+++ js/function.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function(){ // APLICANDO A FUNÇÃO DO MENU MOBILE
 
 	$('.mobile').click(function(){
 
@@ -37,5 +37,151 @@ $(function(){
 			);
 	})	
 
+
+//-----------------------------------------------------------//
+
+	// APLICANDO AS FUNÇÕES DO SLIDE 1
+
+	var indiceAtual = 0;
+	var indiceMaximo = $('.slider1 img').length; 
+	var delay = 5000;
+
+	initSlider();
+
+	cliqueSlider();
+
+		function initSlider(){
+			for(var i = 0; i < indiceMaximo; i++){
+				if(i == 0){
+					$('.bullets-nav1').append('<span style="scale:1.2;"></span>')
+				}else{
+					$('.bullets-nav1').append('<span></span>')
+				}
+			}
+
+			$('.slider1 img').eq(0).fadeIn();
+			setInterval(function(){
+				alternarSlider();
+			},delay);
+		}
+
+		function cliqueSlider (){
+			$('.bullets-nav1 span').click(function(){
+				$('.slider1 img').eq(indiceAtual).stop().fadeOut(2000);
+				indiceAtual = $(this).index();
+				$('.slider1 img').eq(indiceAtual).stop().fadeIn(2000);
+				$('.bullets-nav1 span').css('scale','1');
+				$(this).css('scale','1.2');
+			})
+		}
+
+		function alternarSlider(){
+			$('.slider1 img').eq(indiceAtual).stop().fadeOut(2000);
+			indiceAtual+=1;
+			if(indiceAtual == indiceMaximo)
+				indiceAtual = 0;
+			$('.bullets-nav1 span').css('scale','1');
+			$('.bullets-nav1 span').eq(indiceAtual).css('scale','1.2');
+			$('.slider1 img').eq(indiceAtual).stop().fadeIn(2000);
+
+		}
+})
+//-----------------------------------------------------------//
+
+$(function(){ 	// APLICANDO AS FUNÇÕES DO SLIDE 2
+
+		var indiceAtual = 0;
+	var indiceMaximo = $('.slider2 img').length; 
+	var delay = 5000;
+
+	initSlider();
+
+	cliqueSlider();
+
+		function initSlider(){
+			for(var i = 0; i < indiceMaximo; i++){
+				if(i == 0){
+					$('.bullets-nav2').append('<span style="scale:1.2;"></span>')
+				}else{
+					$('.bullets-nav2').append('<span></span>')
+				}
+			}
+
+			$('.slider2 img').eq(0).fadeIn();
+			setInterval(function(){
+				alternarSlider();
+			},delay);
+		}
+
+		function cliqueSlider (){
+			$('.bullets-nav2 span').click(function(){
+				$('.slider2 img').eq(indiceAtual).stop().fadeOut(2000);
+				indiceAtual = $(this).index();
+				$('.slider2 img').eq(indiceAtual).stop().fadeIn(2000);
+				$('.bullets-nav2 span').css('scale','1');
+				$(this).css('scale','1.2');
+			})
+		}
+
+		function alternarSlider(){
+			$('.slider2 img').eq(indiceAtual).stop().fadeOut(2000);
+			indiceAtual+=1;
+			if(indiceAtual == indiceMaximo)
+				indiceAtual = 0;
+			$('.bullets-nav2 span').css('scale','1');
+			$('.bullets-nav2 span').eq(indiceAtual).css('scale','1.2');
+			$('.slider2 img').eq(indiceAtual).stop().fadeIn(2000);
+
+		}
+
+})
+
+//-----------------------------------------------------------//
+
+$(function(){ 	// APLICANDO AS FUNÇÕES DO SLIDE 3
+
+	var indiceAtual = 0;
+	var indiceMaximo = $('.slider3 img').length; 
+	var delay = 5000;
+
+	initSlider();
+
+	cliqueSlider();
+
+		function initSlider(){
+			for(var i = 0; i < indiceMaximo; i++){
+				if(i == 0){
+					$('.bullets-nav3').append('<span style="scale:1.2;"></span>')
+				}else{
+					$('.bullets-nav3').append('<span></span>')
+				}
+			}
+
+			$('.slider3 img').eq(0).fadeIn();
+			setInterval(function(){
+				alternarSlider();
+			},delay);
+		}
+
+		function cliqueSlider (){
+			$('.bullets-nav3 span').click(function(){
+				$('.slider3 img').eq(indiceAtual).stop().fadeOut(2000);
+				indiceAtual = $(this).index();
+				$('.slider3 img').eq(indiceAtual).stop().fadeIn(2000);
+				$('.bullets-nav3 span').css('scale','1');
+				$(this).css('scale','1.2');
+			})
+		}
+
+		function alternarSlider(){
+			$('.slider3 img').eq(indiceAtual).stop().fadeOut(2000);
+			indiceAtual+=1;
+			if(indiceAtual == indiceMaximo)
+				indiceAtual = 0;
+			$('.bullets-nav3 span').css('scale','1');
+			$('.bullets-nav3 span').eq(indiceAtual).css('scale','1.2');
+			$('.slider3 img').eq(indiceAtual).stop().fadeIn(2000);
+
+		}
 
 })
